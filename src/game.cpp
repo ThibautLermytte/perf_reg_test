85c98abe8331d83eea18ff28b061fa479c23390f
ThibautLermytte <lermytte.t@protonmail.com>
2025-03-21 20:53:24 +0100

    changes

---
+++ src/game.cpp
@@ -0,0 +1,14 @@
+#include "play_game.h"
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[]) {
+	if (argc > 1 && std::isdigit(argv[0][1]) {
+		int i{std::atoi(argv[1]);
+
+		play_game(i, true);
+	} else {
+		std::cout << "Please, enter a positive integer to play..." << std::endl;
+	}
+	return 0;
+}
